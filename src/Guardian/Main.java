b0e52cc3144c234cb69af15789481fa4b98d9c2e
dominick038 <dominickherweijer@gmail.com>
2024-01-15 18:41:54 +0100

    Added a record that functions as the structure to hold all the connection settings

--- src/Guardian/Main.java
+++ src/Guardian/Main.java
@@ -1,7 +1,10 @@
 package Guardian;
+
 public class Main {
+    public record DatabaseConnectionSettings(String host, int port, String usr, String pass, String db) {}
+
     public static void main(String[] args) throws Exception {
-        FileReader file_reader = new FileReader();        
+        GuardianSettingsLoader settingsLoader = new GuardianSettingsLoader();        
         
     }
 }
